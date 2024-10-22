680540f9ab8775a93f0f2ed243c47b0a50b5a9ae
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-10-22 11:12:45 +0900

    1주차 강의 13~14(완강)

--- scc-memory-matching-game-main/lib/src/card_boards.dart
+++ scc-memory-matching-game-main/lib/src/card_boards.dart
@@ -1,60 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:memory_matching_game/src/card.dart';
+import 'package:memory_matching_game/src/card_model.dart';
 
 class CardBoards extends StatefulWidget {
-  CardBoards({super.key});
+  final Function() updateTryCount;
+  CardBoards({super.key, required this.updateTryCount});
 
   @override
   State<CardBoards> createState() => _CardBoardsState();
 }
 
 class _CardBoardsState extends State<CardBoards> {
-  List<int> cards = [1, 5, 2, 6, 3, 4, 3, 2, 6, 1, 4, 5];
-
-  List<bool> cardsFlippedState = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ];
-  int instantFirstCard = -1;
-
-  void onTapCard(int cardIndex) {
-    print('$cardIndex 번째 카드를 선택하셨습니다.');
-
-    if (instantFirstCard == -1) {
-      instantFirstCard = cardIndex;
-    } else {
-      // 두번째 카드가 선택되었을때 로직 추가
-      var firstCard = cards[instantFirstCard];
-      var secondCard = cards[cardIndex];
-      if (firstCard == secondCard) {
-        print('짝이 맞았습니다.');
-        instantFirstCard = -1; // 추가
-      } else {
-        resetInstantCards(instantFirstCard, cardIndex);
-      }
-    }
-    setState(() {
-      cardsFlippedState[cardIndex] = true;
+  late List<CardModel> cards;
+  @override
+  void initState() {
+    super.initState();
+    List<int> cardsValue = [1, 5, 2, 6, 3, 4, 3, 2, 6, 1, 4, 5];
+    cardsValue.shuffle();
+    cards = List.generate(cardsValue.length, (index) {
+      return CardModel(index: index, cardValue: cardsValue[index]);
     });
   }
 
-  void resetInstantCards(int firstIndex, int secondIndex) async {
+  CardModel? instantFirstCard;
+
+  void onTapCard(int cardIndex) {
+    print('$cardIndex 번째 카드를 선택하셨습니다.');
+    if (instantFirstCard == null) {
+      instantFirstCard = cards[cardIndex];
+    } else {
+      // 두번째 카드가 선택되었을때 로직 추가
+      widget.updateTryCount();
+      var firstCard = instantFirstCard;
+      var secondCard = cards[cardIndex];
+      if (firstCard!.cardValue == secondCard.cardValue) {
+        print('짝이 맞았습니다.');
+        instantFirstCard = null;
+      } else {
+        resetInstantCards(instantFirstCard!, secondCard);
+      }
+    }
+    setState(() {
+      cards[cardIndex].setFlipped(true);
+    });
+  }
+
+  void resetInstantCards(CardModel firstCard, CardModel secondCard) async {
     await Future.delayed(Duration(seconds: 2));
     setState(() {
-      cardsFlippedState[instantFirstCard] = false; // 추가
-      cardsFlippedState[secondIndex] = false; // 추가
+      firstCard.setFlipped(false);
+      secondCard.setFlipped(false);
     });
-    instantFirstCard = -1; // 추가
+    instantFirstCard = null;
     return;
   }
 
@@ -67,9 +64,11 @@ class _CardBoardsState extends State<CardBoards> {
         children: [
           for (var i = 0; i < cards.length; i++)
             CardWidget(
-                cardNumber: cards[i],
-                isFlipped: cardsFlippedState[i],
-                onTap: () => onTapCard(i)),
+              card: cards[i],
+              onTap: () {
+                onTapCard(i);
+              },
+            ),
         ],
       ),
     );
